e5ac5c347c735420508173fd750a4739b346fc69
sonal <sonalkarle01@gmail.com>
2021-03-17 22:18:29 +0530

    Retrive all data from Json file

--- RestSharp/UnitTest1.cs
+++ RestSharp/UnitTest1.cs
@@ -38,64 +38,7 @@ namespace Emppayrollrest
             Assert.AreEqual(13, list.Count);
             
         }
-        /// <summary>
-        /// Uc3: Add employee in to the list
-        /// </summary>
-        [Test]
-        public void GivenEmployee_onpost_shouldreturnAddEmployee()
-        {
-            RestRequest request = new RestRequest("/Emppayroll", Method.POST);
-            JObject jObject = new JObject();
-            jObject.Add("name", "Sona");
-            jObject.Add("Gender", "Female");
-            jObject.Add("Salary", 20000);
-            jObject.Add("StartDate", "29 oct 2019");
-            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
-
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
-            Employee dataresponse = JsonConvert.DeserializeObject<Employee>(response.Content);
-            Assert.AreEqual("Sona", dataresponse.name);
-            Assert.AreEqual(20000, dataresponse.salary);
-           
-            Console.WriteLine(response.Content);
-            
-        }
-        /// <summary>
-        /// UC4:UC4: Edit the details of employee
-        /// </summary>
-        [Test]
-        public void GivenEmployee_OnUpdate_shouldreturn_Updateemployee()
-        {
-            RestRequest request = new RestRequest("/Emppayroll/12", Method.PUT);
-            JObject jObject = new JObject();
-            jObject.Add("name", "Sonal Karle");
-            jObject.Add("Gender", "Female");
-
-            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
-
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
-            Employee dataresponse = JsonConvert.DeserializeObject<Employee>(response.Content);
-            Assert.AreEqual("Sonal Karle", dataresponse.name);
-            Assert.AreEqual("Female", dataresponse.gender);
-            Console.WriteLine(response.Content);
-        }
-        /// <summary>
-        /// UC5:Delete the person details
-        /// </summary>
-        [Test]
-        public void GivenEmployeeID_OnDelete_shouldreturnsucessFulstatus()
-        {
-            RestRequest request = new RestRequest("/Emppayroll/14", Method.DELETE);
-          
-
-            IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
-            Console.WriteLine(response.Content);
-
-        }
-        
+       
        
         
     }
